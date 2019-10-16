2c9c00ab1532bd30d6990dc39e5b4ce73f3c7d6b
rideregghost <46668765+rideregghost@users.noreply.github.com>
2019-10-16 14:59:19 +0530

    Update os_project1.c

--- os_project1.c
+++ os_project1.c
@@ -35,10 +35,34 @@ int sort(struct process *p,int n,int type){
                     p[j+1]=temp;
                 }
     }
+    else if(type==4){
+        for(int i=0;i<n-1;i++)
+            for(int j=0;j<n-i-1;j++)
+                if(p[j].process_id>p[j+1].process_id){
+                    temp=p[j];
+                    p[j]=p[j+1];
+                    p[j+1]=temp;
+                }
+    }
     return(1);
 }
+int print_pretty(struct process *p,int n,int t){
+    float avg_wt=0,avg_tt=0;
+    sort(p,n,4);
+    printf("\nProcess_id\tWaitting Time\tTurnaround Time\n");
+    for(int i;i<n;i++){
+        avg_wt+=p[i].waitting_time;
+        avg_tt+=p[i].turnaround_time;
+        printf("\tP%d\t\t%d\t\t%d\n",p[i].process_id,p[i].waitting_time,p[i].turnaround_time);
+    }
+    avg_wt=(float)avg_wt/n;
+    avg_tt=(float)avg_tt/n;
+    printf("\tAverage Waitting Time:- %.2f\n",avg_wt);
+    printf("\tAverage Turnaround Time:- %.2f\n",avg_tt);
+    printf("\tThroughput:- %f\n",(float)t/n);
+}
 int returnIndex(struct process *p,int n,int a){
-    for(int i=0;i<n;i++){  
+    for(int i=0;i<n;i++){
         if(p[i].process_id==a)
         return(i);
     }
@@ -52,6 +76,7 @@ int fcfs(struct process *que1,int n){
         que1[i].turnaround_time=total_time;
         printf("-P%d-",que1[i].process_id);
     }
+    print_pretty(que1,n,total_time);
     return(1);
 }
 int sjf(struct process *que1,int n){
@@ -63,6 +88,7 @@ int sjf(struct process *que1,int n){
         que1[i].turnaround_time=total_time;
         printf("-P%d-",que1[i].process_id);
     }
+    print_pretty(que1,n,total_time);
     return(1);
 }
 int rr(struct process *que1,int n){
@@ -74,18 +100,23 @@ int rr(struct process *que1,int n){
     while(n1){
         if(que1[count].remaining_time<=time_quanta && que1[count].remaining_time>0){
             total_time+=que1[count].remaining_time;
-            que1[count++].remaining_time=0;
-            printf("-P%d-",que1[count].process_id);
+            que1[count].remaining_time=0;
+            que1[count].turnaround_time=total_time;
+            que1[count].waitting_time=total_time-que1[count].burst_time;
+            printf("-P%d-",que1[count++].process_id);
             n1--;
         }
         else if(que1[count].remaining_time>0){
-            que1[count++].remaining_time-=time_quanta;
+            que1[count].remaining_time-=time_quanta;
             total_time+=time_quanta;
-            printf("-P%d-",que1[count].process_id);
+            printf("-P%d-",que1[count++].process_id);
         }
+        else
+            count++;
         if(count==n)
-            count%=n;
+            count=count%n;
     }
+    print_pretty(que1,n,total_time);
     return(1);
 }
 int priority(struct process *que1,int n){
@@ -97,6 +128,7 @@ int priority(struct process *que1,int n){
         que1[i].turnaround_time=total_time;
         printf("-P%d-",que1[i].process_id);
     }
+    print_pretty(que1,n,total_time);
     return(1);
 }
 int multiLevelScheduling(struct process *que1,int n){
@@ -144,16 +176,15 @@ int multiLevelScheduling(struct process *que1,int n){
         que1=que2;
         n1=n2;
     }
+    print_pretty(p,n,total_time);
     return(1);
 }
-/*int print_pretty(int *a){
 
-}*/
 int main(){
     int c,n;
     while(1){
         printf("\n\t\tAvailable Scheduling Algorithms\n\t1. FCFS(first come first served)\n\t2. SJF(shortest job first)\n\t3. RR(round robin)\n");
-        printf("\t4. Priority based\n\t5. Modified multilevel(our propossed alorithm\n\t6. Exit\nSelect any one option:-\t");
+        printf("\t4. Priority based\n\t5. Modified multilevel(our propossed alorithm)\n\t6. Exit\nSelect any one option:-\t");
         scanf("%d",&c);
         if(c<6){
             printf("Enter the number of processes:-\t");
@@ -172,7 +203,7 @@ int main(){
                     scanf("%d %d",&queue1[i].burst_time,&queue1[i].priority);
                     queue1[i].remaining_time=queue1[i].burst_time;
                     queue1[i].process_id=i+1;
-                }     
+                }
             switch (c){
                 case 1: fcfs(queue1,n);
                         break;
@@ -184,7 +215,7 @@ int main(){
                         break;
                 case 5: multiLevelScheduling(queue1,n);
                         break;
-                default: printf("Wrong option selected");        
+                default: printf("Wrong option selected");
             }
         }
         else{
